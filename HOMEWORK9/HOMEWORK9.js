84ce9710261400406cccd644dde70470b9aaefe3
Aleksey <example@gmail.com>
2021-02-22 16:38:22 +0200

    HOMEWORK9

---
+++ HOMEWORK9/HOMEWORK9.js
@@ -0,0 +1,21 @@
+//Task 1
+//$("a[href^='https://'").attr('target', '_blank');
+
+//Task 2
+
+// let $getHead = $("h2.head");
+// let $getInner = $getHead.children(".inner");
+
+// $getHead.css("background-color", "red");
+// $getInner.css("font-size", "36px");
+
+//Task 3
+
+// $(document).ready(function() {        
+//   content1 = $('h3').html();
+//   content2 = $('div').html();
+//   $('h3').html(content2).show();
+//   $('div').html(content1).show();
+// });
+
+//Task 4
